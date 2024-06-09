d91825470724cbcb8d8280abd5d3c73cb0b03ddf
Pedro Oliveira <Pedroaolive@gmail.com>
2024-06-09 18:26:39 -0300

    feat: add route to test conection

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -2,6 +2,10 @@ import { FastifyInstance } from "fastify";
 import { registerClinic } from "./modules/clinic/controllers/registerClinic";
 
 export const appRoutes = async (app: FastifyInstance) => {
-    // Rotas clinicas
-    app.post("/clinics", registerClinic);
-}
+  app.get("/", () => {
+    return "Conexão API Care4You!";
+  });
+
+  // Rotas clinicas
+  app.post("/clinics", registerClinic);
+};
