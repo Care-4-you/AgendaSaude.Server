e245aea6928ebad02375f0a7881841f6177c7dac
Pedro Oliveira <Pedroaolive@gmail.com>
2024-11-04 21:07:49 -0300

    test: testing send e-mail with nodemailer

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -3,10 +3,36 @@ import { FastifyInstance } from "fastify";
 import { registerClinic } from "./controllers/clinic/registerClinic";
 import { pacientRoutes } from "./routes/pacientRoutes";
 import { clinicRoutes } from "./routes/clinicRoutes";
+import { createTransport } from "nodemailer";
 
 export const appRoutes = async (app: FastifyInstance) => {
   app.get("/", () => {
     return "Conexão API Care4You!";
+  });
+
+  app.get("/teste", () => {
+    const transport = createTransport({
+      host: "smtp.mail.yahoo.com",
+      port: 465,
+      secure: true, // true para porta SSL
+      auth: {
+        user: "care4you.validation@yahoo.com",
+        pass: "!4lexanderFl3m1ng!",
+      },
+    });
+
+    transport
+      .sendMail({
+        from: "care4you.validation@yahoo.com",
+        to: "pedroaolive@gmail.com",
+        subject: "Enviando e-mail",
+        //html: "",
+        text: "Teste",
+      })
+      .then((response) => console.log("E-mail enviado com sucesso!"))
+      .catch((error) =>
+        console.error(`Error ao envio do email: ${error.message}`),
+      );
   });
 
   await app.register(pacientRoutes);
