063e41d420f2e7000cf35a3a58879cfaccf70e10
CodeDeivid <deividmickael@gmail.com>
2025-03-10 20:52:50 -0300

    feat: adicionar rotas para ativação de clínicas e reenvio de email de ativação

--- src/http/routes/clinicRoutes.ts
+++ src/http/routes/clinicRoutes.ts
@@ -1,7 +1,11 @@
 import { FastifyInstance } from "fastify";
 import { verifyJwt } from "../middlewares/verify-jwt";
 import { registerClinic } from "../controllers/clinic/registerClinic";
+import { activateClinic } from "../controllers/clinic/activateClinic";
+import { resendActivationEmail } from "../controllers/clinic/resendActivationEmail";
 
 export const clinicRoutes = async (app: FastifyInstance) => {
   app.post("/clinics", registerClinic);
+  app.get("/clinics/activate", activateClinic);
+  app.post("/clinics/resend-activation", resendActivationEmail);
 };
